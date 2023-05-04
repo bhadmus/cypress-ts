82758631f0538e188d31d8e84846ae7d4c11976e
Ademola Bhadmus <sanguine@Ademolas-MacBook-Pro.local>
2023-05-04 15:48:59 +0100

    code cleanup

--- cypress/integration/Amazon/MySteps.spec.ts
+++ cypress/integration/Amazon/MySteps.spec.ts
@@ -2,34 +2,6 @@ import { Given, When } from "cypress-cucumber-preprocessor/steps";
 
 Given(/^I am on the Amazon page$/, () => {
     cy.visit('/')
-    // cy.request('https://www.virginmedia.com/dpb/routes.json').then((resp) => {
-    //     cy.log(resp.body)
-    //     var jsonData = JSON.stringify(resp.body)
-    //     cy.log(typeof (jsonData))
-    //     cy.writeFile("cypress/fixtures/routes.json", jsonData)
-    //     cy.fixture("routes").then((routes) => {
-    //         for (var i = 0; i < routes.length; i++) {
-    //             cy.visit('/' + routes)
-    //             let firstTitle = 'title';
-    //             let firstHeader = 'h1';
-    //             let body = 'body';
-    //             let footer = "#footer";
-    //
-    //             cy.get(firstTitle, { timeout: 30000 }).should('exist');
-    //             cy.get(firstHeader, { timeout: 30000 }).should('exist');
-    //             cy.get(body, { timeout: 30000 }).should('exist');
-    //             cy.get(footer, { timeout: 30000 }).should('exist');
-    //             cy.get(firstTitle).should('not.contains.text', 'message');
-    //
-    //             // cy.window().then((win) => {
-    //             //     expect(win.console.error).to.have.callCount(0);
-    //             //     //expect(win.console.warn).to.have.callCount(0);
-    //             // })
-    //         }
-    //
-    //     })
-    //
-    // })
 
 });
 When(/^I insert a search item$/, () => {
